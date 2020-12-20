e98274e8fb0e5f4519f3506cda48c76d11f8beda
SenorGrande <clooneyjh@gmail.com>
2020-12-20 18:51:47 +1300

    Adding workspace path to report filepath This should be passed as an argument to the JS script

--- generate-pdf.js
+++ generate-pdf.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.goto('file:///report.html', {waitUntil: 'networkidle2'});
+    await page.goto('file:///github/workspace/report.html', {waitUntil: 'networkidle2'});
     await page.pdf({path: 'report.pdf', format: 'A4'});
 
     await browser.close();
